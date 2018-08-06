395de2e4e8176718dfa0a7d8755f3113ff625875
nancychu <colachu@139.com>
2018-08-06 22:43:02 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 import os,sys
 
-path = 'F:/project/BI_LogTest/FTP/zhj'
+path = 'D:\PyProject\BI_Pro-master\BI_Pro-master\FTP'
 filenames = []
-for parent,dirnames,basefilenames in os.walk(path):
+for parent, dirnames, basefilenames in os.walk(path):
 
     for filename in basefilenames:
         if filename.endswith('.log') or filename.endswith('.txt'):
             filenames.append(filename)
+
+            # 防止文件名中包含.  https://www.jianshu.com/p/d6ccc8d9ff73
+        fullist = filename.split('.')
+        print('fullist: ')
+        print(fullist)
+        namelist = fullist[:-1]  # list切片
+        print('namelist: ')
+        print(namelist)
+        file_name = ''
+        for i in namelist:
+            file_name = file_name + i + '.'
+            print(file_name)
+
 print(filenames)
 print(len(filenames))
